98dcfbfefa34aed3d4563e8184c5f7d476958b02
Alejandro Greggio <agreggio@slingr.io>
2024-01-22 14:09:07 -0300

    #1 - Task: Create gmail package

--- flowSteps/apiCallGmail/step.js
+++ flowSteps/apiCallGmail/step.js
@@ -59,6 +59,7 @@ step.apiCallGmail = function (inputs) {
 
 	options= setApiUri(options);
 	options= setRequestHeaders(options);
+	options= setAuthorization(options);
 
 	switch (inputsLogic.method.toLowerCase()) {
 		case 'get':
@@ -69,16 +70,11 @@ step.apiCallGmail = function (inputs) {
 			return httpService.delete(options);
 		case 'put':
 			return httpService.put(options);
-		case 'connect':
-			return httpService.connect(options);
 		case 'head':
 			return httpService.head(options);
 		case 'options':
 			return httpService.options(options);
 		case 'patch':
-			return httpService.patch(options);
-		case 'trace':
-			return httpService.trace(options);
 	}
 
 	return null;
@@ -117,91 +113,48 @@ function stringToObject (obj) {
 	return null;
 }
 
+/****************************************************
+ Private API
+ ****************************************************/
+
 function setApiUri(options) {
-	var API_URL = config.get("Gmail_API_BASE_URL");
-	var url = options.path || "";
-	options.url = API_URL + url;
-	sys.logs.debug('[Gmail] Set url: ' + options.path + "->" + options.url);
+	let url = options.path || "";
+	options.url = config.get("GMAIL_API_BASE_URL") + url;
+	sys.logs.debug('[gmail] Set url: ' + options.path + "->" + options.url);
 	return options;
 }
 
 function setRequestHeaders(options) {
-	var headers = options.headers || {};
-
-	sys.logs.debug('[Gmail] Set header Bearer');
+	let headers = options.headers || {};
 	headers = mergeJSON(headers, {"Content-Type": "application/json"});
-	headers = mergeJSON(headers, {"Authorization": "Bearer "+getAccessTokenForAccount()});
-
-	if (headers.Accept === undefined || headers.Accept === null || headers.Accept === "") {
-		sys.logs.debug('[Gmail] Set header accept');
-		headers = mergeJSON(headers, {"Accept": "application/json"});
-	}
-
 	options.headers = headers;
 	return options;
 }
 
-function getAccessTokenForAccount(account) {
-	account = account || "account";
-	sys.logs.info('[Gmail] Getting access token for account: '+account);
-	var installationJson = sys.storage.get('installationInfo-Gmail---'+account) || {id: null};
-	var token = installationJson.token || null;
-	var expiration = installationJson.expiration || 0;
-	if (!token || expiration < new Date().getTime()) {
-		sys.logs.info('[Gmail] Access token is expired or not found. Getting new token');
-		var res = httpService.post(
-			{
-				url: "https://oauth2.googleapis.com/token",
-				headers: {
-					'Content-Type': 'application/x-www-form-urlencoded'
-				},
-				body: {
-					grant_type: 'urn:ietf:params:oauth:grant-type:jwt-bearer',
-					assertion: getJsonWebToken()
-				}
-			});
-		token = res.access_token;
-		var expires_at = res.expires_in;
-		expiration = new Date(new Date(expires_at) - 1 * 60 * 1000).getTime();
-		installationJson = mergeJSON(installationJson, {"token": token, "expiration": expiration});
-		sys.logs.info('[Gmail] Saving new token for account: ' + account);
-		sys.storage.replace('installationInfo-Gmail---'+account, installationJson);
-	}
-	return token;
+function setAuthorization(options) {
+	let authorization = options.authorization || {};
+	sys.logs.debug('[gmail] setting authorization');
+	let pkgConfig = config.get();
+	sys.logs.debug('[gmail] config: '+JSON.stringify(pkgConfig));
+	sys.logs.debug('[gmail] config id: '+JSON.stringify(pkgConfig.id));
+
+	authorization = mergeJSON(authorization, {
+		type: "oauth2",
+		accessToken: sys.storage.get(pkgConfig.id + ' - access_token',{decrypt:true}),
+		headerPrefix: "Bearer"
+	});
+	options.authorization = authorization;
+	return options;
 }
 
-function getJsonWebToken() {
-	var currentTime = new Date().getTime();
-	var futureTime = new Date(currentTime + ( 10 * 60 * 1000)).getTime();
-	var scopeProp= config.get("scope");
-	var scopes;
-	if (!!scopeProp) {
-		scopes = scopeProp.map(function (s) {
-			return "https://www.googleapis.com/auth/" + s;
-		});
-	}
-	var scopesGlobal = scopes.join(" ");
-	return sys.utils.crypto.jwt.generate(
-		{
-			iss: config.get("serviceAccountEmail"),
-			aud: GOOGLEWORKSPACE_API_AUTH_URL,
-			scope: scopesGlobal,
-			iat: currentTime,
-			exp: futureTime
-		},
-		config.get("privateKey"),
-		"RS256"
-	)
-}
-
-function mergeJSON (json1, json2) {
-	var result = {};
-	var key;
+function mergeJSON(json1, json2) {
+	const result = {};
+	let key;
 	for (key in json1) {
-		if(json1.hasOwnProperty(key)) result[key] = json1[key];
+		if (json1.hasOwnProperty(key)) result[key] = json1[key];
 	}
 	for (key in json2) {
-		if(json2.hasOwnProperty(key)) result[key] = json2[key];
+		if (json2.hasOwnProperty(key)) result[key] = json2[key];
 	}
 	return result;
 }
